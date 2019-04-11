eb4989bbc1c27af074b2813faa5f35fad43e5bdb
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2019-04-11 14:47:52 +1000

    Created /profile route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   resources :posts
   resources :users
-
-  # get 'users/edit'
-  # update 'users/'
+  get '/profile', to: 'users#index'
   
   root 'pages#welcome'
 
-  get '/profile', to: 'users#profile'
+  get '/profile', to: 'users#index'
   
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
