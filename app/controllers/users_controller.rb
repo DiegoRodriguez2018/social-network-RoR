4b1c4e8b24207a7288072ee6e25dfdd0b164605b
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2019-04-11 14:26:03 +1000

    Implemented profile editing logic for User.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  # user = User.find(params[:id])
+  def index
+    @user = current_user 
+    @message = "Welcome to your profile"
+  end
+
+  def edit
+    @user = current_user 
+  end
+
+  # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+    @user = current_user
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = user.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :lastname)
+    end
+end
