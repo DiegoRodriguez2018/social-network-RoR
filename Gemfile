3f1464f5db776aba9cd6cd3cb301353550ca3841
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2019-04-17 10:03:27 +1000

    Installed bootstrap 4.

--- Gemfile
+++ Gemfile
@@ -40,6 +40,11 @@ gem 'devise', '~> 4.6', '>= 4.6.2'
 
 gem 'pundit', '~> 2.0', '>= 2.0.1'
 
+# Bootstrap
+gem 'bootstrap', '~> 4.3.1'
+
+gem 'jquery-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :users
-  get '/profile', to: 'users#index'
-  
-  root 'pages#welcome'
-
-  get '/profile', to: 'users#index'
-  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'users#index'
+  get '/profile', to: 'users#index'
+  resources :posts
+  resources :users
 end
